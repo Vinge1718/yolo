0879f6d6a9c63f5b782a8e3c0681ba4431863ff8
Brillianne <brillianchepngeno@gmail.com>
2023-04-06 18:05:48 +0300

    add mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+let mongodb_url = 'mongodb+srv://brillianne:lenovo@cluster0.fbyztjh.mongodb.net/?retryWrites=true&w=majority';
+let dbName = 'darkroom';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
