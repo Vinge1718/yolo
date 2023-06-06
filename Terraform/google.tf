bc4ae9354ed564c2bed164666a7b7cf3aa02e995
Martha Jepkemoi Ngaruiya <rimash2@yahoo.com>
2023-06-06 20:19:52 +0300

    created an instance-Test

---
+++ Terraform/google.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  project     = "subtle-creek-386806"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+}
