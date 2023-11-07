8f228dc9f8830210cb3918f2cf7188b633735908
RAJABIBRAZ <akilkil5@gmail.com>
2023-11-07 07:54:21 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,16 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+// let mongodb_url = 'mongodb://localhost/';
+// let mongodb_url = 'mongodb://yolo-database-container:56081/yoloip';
+// let mongodb_url = 'mongodb://localhost:56081/yoloip';
+// let mongodb_url = 'mongodb://mongo:56081/yoloip';
+let mongodb_url = 'mongodb+srv://rajibraz:NRTRsNgIuS7p15M8@cluster0.counwdf.mongodb.net/?retryWrites=true&w=majority/yoloip';
+// let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+// const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+const MONGODB_URI = process.env.MONGODB_URI || mongodb_url
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
 
